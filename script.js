fd1526c864ffa50564228a7e5bf0ca6574cba18b
karma-chameleon22 <quarantinedegg@gmail.com>
2026-07-20 22:39:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-console.log("BLAST SCRIPT IS WORKING");
+document.addEventListener("DOMContentLoaded", function(){
 
-alert("BLAST SCRIPT LOADED");
+    console.log("DOM loaded");
+
+
+    const button = document.getElementById("generateButton");
+
+
+    if(button){
+
+        console.log("Generate button found");
+
+
+        button.onclick = function(){
+
+            console.log("Generate button clicked");
+
+
+            document.getElementById("result").value =
+            "BLAST BUTTON WORKS";
+
+        };
+
+    }
+
+    else {
+
+        console.log("Generate button NOT found");
+
+    }
+
+
+});
